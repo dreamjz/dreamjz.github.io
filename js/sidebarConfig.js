01312307d30415c40631f0d8959dc01be0a269da
kesa <2356216441@qq.com>
2021-11-07 22:05:56 +0800

    deploy 2021-11-07_22:05:56

--- js/sidebarConfig.js
+++ js/sidebarConfig.js
@@ -1,31 +1,17 @@
-const sidebarLinux = require("./sbarLinux");
-const sidebarGolang = require("./sbarGolang");
-const sidebarNginx = require("./sbarNginx");
-const sidebarDocker = require("./sbarDocker");
-const sidebarFrontEnd = require("./sbarFrontEnd");
-const sidebarRaspi = require("./sbarRaspi");
+const sidebarLinux = require('./sbarLinux');
+const sidebarGolang = require('./sbarGolang');
+const sidebarNginx = require('./sbarNginx');
+const sidebarDocker = require('./sbarDocker');
+const sidebarFrontEnd = require('./sbarFrontEnd');
+const sidebarRaspi = require('./sbarRaspi');
 module.exports = {
-  // sidebar: {
-  //   "/zh/": [
-  //     {
-  //       title: "Welcome",
-  //       path: "/zh/",
-  //     },
-  //     sidebarGolang,
-  //     sidebarLinux,
-  //     sidebarNginx,
-  //     sidebarDocker,
-  //     sidebarFrontEnd,
-  //     sidebarRaspi,
-  //   ],
-  // },
   sidebar: {
-    "/zh/": [
+    '/zh/': [
       {
-        title: "Welcome",
-        path: "/zh/",
-        sidebarDepth: 1,
-        collapsable: false,
+        title: 'Welcome',
+        sidebarDepth: 2,
+        collapsable: true,
+        path: '/zh/',
       },
       sidebarGolang,
       sidebarLinux,
