46c5970b18c88aa3f3938e16d17c33dcb46afb8d
kesa <2356216441@qq.com>
2021-11-07 16:36:56 +0800

    deploy 2021-11-07_16:36:56

--- js/sbarDocker.js
+++ js/sbarDocker.js
@@ -1,8 +1,8 @@
 module.exports = {
   title: 'Docker',
   children: [
-    '/zh/docker/docker-tutorial/get-started',
-    '/zh/docker/docker-tutorial/docker-nginx',
+    '/zh/docker/get-started',
+    '/zh/docker/docker-nginx',
     {
       title: 'Docker practice',
       sidebarDepth: 2,
