725c2491debb8906079c2b5c7fedeb6784893ef1
kesa <2356216441@qq.com>
2021-12-09 21:55:44 +0800

    deploy 2021-12-09_21:55:44

--- js/sbarFrontEnd.js
+++ js/sbarFrontEnd.js
@@ -3,6 +3,12 @@ module.exports = {
   children: [
     {
       title: 'Vue',
+      collapsable: true,
+      children: [
+        '/zh/frontend/vue/vue-cli',
+        '/zh/frontend/vue/vue-router',
+        '/zh/frontend/vue/vuex',
+      ]
     },
   ],
 };
